679834689bb95fc9f52200f2667edc41db171ade
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2023-07-06 11:47:37 +0900

    ui 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* header */}
+      <div style={{ backgroundColor: "#33FFE0" }}>투두리스트</div>
+      {/* input */}
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <button>추가하기</button>
+      </div>
+      {/* body */}
+      <div>
+        {/* isWorking */}
+        <div>
+          <h3>해라</h3>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                border: "1px solid black",
+                width: "200px",
+                height: "200px",
+                marginRight: "12px",
+              }}
+            >
+              <h4>제목</h4>
+              <p>내용내용내용내용</p>
+              <button>완료</button>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+            <div
+              style={{
+                border: "1px solid black",
+                width: "200px",
+                height: "200px",
+              }}
+            >
+              <h4>제목</h4>
+              <p>내용내용내용내용</p>
+              <button>완료</button>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </div>
+
+        {/* isDone */}
+        <div>
+          <h3>했다</h3>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                border: "1px solid black",
+                width: "200px",
+                height: "200px",
+                marginRight: "12px",
+              }}
+            >
+              <h4>제목</h4>
+              <p>내용내용내용내용</p>
+              <button>취소</button>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+            <div
+              style={{
+                border: "1px solid black",
+                width: "200px",
+                height: "200px",
+              }}
+            >
+              <h4>제목</h4>
+              <p>내용내용내용내용</p>
+              <button>취소</button>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
