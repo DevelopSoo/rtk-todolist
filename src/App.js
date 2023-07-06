82ed4a40acbce2ee753b3416c1d7899b37ab711e
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2023-07-06 15:54:25 +0900

    update: 리덕스툴킷으로 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import { nanoid } from "nanoid";
+import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  리덕스추가하는함수,
+  리덕스삭제함수,
+  리덕스완료취소함수,
+} from "./store";
 
 function App() {
   const [입력값들, set입력값들] = useState({
@@ -8,30 +14,22 @@ function App() {
     내용: "",
   });
 
-  const [투두리스트, set투두리스트] = useState([
-    { id: nanoid(), 제목: "리액트 공부", 내용: "빡세게 공부", 완료여부: false },
-    { id: nanoid(), 제목: "국어 공부", 내용: "열심히 공부", 완료여부: false },
-    {
-      id: nanoid(),
-      제목: "영어 공부",
-      내용: "너무 어려운 공부",
-      완료여부: true,
-    },
-    { id: nanoid(), 제목: "수학 공부", 내용: "포기", 완료여부: true },
-  ]);
+  // redux toolkit
+  const state = useSelector((state) => {
+    return state.todos;
+  });
+
+  const dispatch = useDispatch();
 
   const 추가하는함수 = (e) => {
     e.preventDefault();
-    set투두리스트((prev) => {
-      return [
-        ...prev,
-        {
-          id: nanoid(),
-          ...입력값들,
-          완료여부: false,
-        },
-      ];
-    });
+    dispatch(
+      리덕스추가하는함수({
+        id: nanoid(),
+        ...입력값들,
+        완료여부: false,
+      })
+    );
   };
 
   const 입력값들변경하는함수 = (e) => {
@@ -42,25 +40,11 @@ function App() {
   };
 
   const 삭제함수 = (id) => {
-    set투두리스트((prev) => {
-      return prev.filter((투두) => 투두.id !== id);
-    });
+    dispatch(리덕스삭제함수(id));
   };
 
   const 완료및취소함수 = (id) => {
-    set투두리스트((prev) => {
-      return prev.map((할일) => {
-        if (할일.id === id) {
-          return {
-            ...할일,
-            완료여부: !할일.완료여부,
-          };
-        }
-        return {
-          ...할일,
-        };
-      });
-    });
+    dispatch(리덕스완료취소함수(id));
   };
 
   return (
@@ -81,7 +65,7 @@ function App() {
         <div>
           <h3>해라</h3>
           <div style={{ display: "flex" }}>
-            {투두리스트
+            {state
               .filter((할일) => !할일.완료여부)
               .map((할일) => (
                 <div
@@ -106,7 +90,7 @@ function App() {
         <div>
           <h3>했다</h3>
           <div style={{ display: "flex" }}>
-            {투두리스트
+            {state
               .filter((할일) => 할일.완료여부)
               .map((할일) => (
                 <div
---
+++ src/store.js
@@ -0,0 +1,40 @@
+// 기본세팅
+import { configureStore, createSlice, nanoid } from "@reduxjs/toolkit";
+
+// redux state 생성
+let 투두리스트 = createSlice({
+  name: "투두리스트",
+  initialState: [
+    { id: nanoid(), 제목: "리액트 공부", 내용: "빡세게 공부", 완료여부: false },
+    { id: nanoid(), 제목: "국어 공부", 내용: "열심히 공부", 완료여부: false },
+    {
+      id: nanoid(),
+      제목: "영어 공부",
+      내용: "너무 어려운 공부",
+      완료여부: true,
+    },
+    { id: nanoid(), 제목: "수학 공부", 내용: "포기", 완료여부: true },
+  ],
+  reducers: {
+    리덕스추가하는함수: (state, action) => {
+      state.push(action.payload);
+    },
+    리덕스삭제함수: (state, action) => {
+      return state.filter((할일) => 할일.id !== action.payload);
+    },
+    리덕스완료취소함수: (state, action) => {
+      const 선택된놈 = state.find((할일) => 할일.id === action.payload);
+      선택된놈.완료여부 = !선택된놈.완료여부;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    todos: 투두리스트.reducer,
+  },
+});
+
+export const { 리덕스추가하는함수, 리덕스삭제함수, 리덕스완료취소함수 } =
+  투두리스트.actions;
+export default store;
